e65fcc64b278dc9c5cea89ff6896e325cb5352ba
Yeicor <yeicor+no-reply@github.com>
2026-04-30 18:54:33 +0200

    Update portfile.cmake

--- vcpkg_ports/openblas/portfile.cmake
+++ vcpkg_ports/openblas/portfile.cmake
@@ -5,7 +5,7 @@ file(COPY "${VCPKG_ROOT_DIR}/ports/openblas/" DESTINATION "${CMAKE_CURRENT_LIST_
 # https://github.com/OpenMathLib/OpenBLAS#support-for-multiple-targets-in-a-single-library
 file(READ "${VCPKG_ROOT_DIR}/ports/openblas/portfile.cmake" upstream_content)
 set(ARCH_TARGET "P2")
-if(CMAKE_HOST_PROCESSOR MATCHES "arm64|aarch64")
+if(CMAKE_HOST_PROCESSOR MATCHES "^aarch64")
   set(ARCH_TARGET "ARMV8")
 endif()
 string(REPLACE "-DBUILD_TESTING=OFF" "-DBUILD_TESTING=OFF -DDYNAMIC_ARCH=ON -DDYNAMIC_OLDER=ON -DTARGET=${ARCH_TARGET}" upstream_content "${upstream_content}")
